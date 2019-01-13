f60a5ee045d43d905d3694463817a916f1a5d8c7
xwcandy <18374836646@163.com>
2019-01-13 11:58:56 +0800

    安装el-ui 完成登录页结构

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div class="login-container">
+    <div class="form-wrap">
+      <h3>用户登录</h3>
+      <!-- 表单 -->
+      <el-form label-position="top" label-width="80px" :model="formData">
+        <el-form-item label="用户名">
+          <el-input v-model="formData.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="formData.password"></el-input>
+        </el-form-item>
+        <el-button type="primary" class='btn-login'>登录</el-button>
+      </el-form>
+
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'app',
+  name: 'login',
+  data() {
+    return {
+      formData: {
+        username: '',
+        password: ''
+      }
+    }
+  },
   components: {
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang='scss'>
+  .login-container {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: skyblue;
+    .form-wrap {
+      padding: 40px;
+      width: 500px;
+      border-radius: 20px;
+      background-color: #fff;
+      .btn-login {
+        width: 100%;
+      }
+    }
+
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+// 导入element-ui的js文件和css
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// use一下
+Vue.use(ElementUI);
+
+// 引入base.css
+import './assets/base.css';
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
